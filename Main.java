d156888533d1e54bd989ceca67ca4566ce83c462
12430373 <102036037+12430373@users.noreply.github.com>
2022-07-03 22:39:05 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,13 @@
-import Nodo.Ejercicio_3;
+import src.Ejercicio_4;
+
+//import Nodo.Ejercicio_3;
 
 //import Nodo.Ejercicio;
 public class Main {
     public static void main(String[] args) {
         //System.out.println(Ejercicio.factorial(5));
-        int vector[]={2,3,9,7,4,1};
-        System.out.println(Ejercicio_3.sumaVector(vector, 5));
+        //int vector[]={2,3,9,7,4,1};
+        //System.out.println(Ejercicio_3.sumaVector(vector, 5));
+        System.out.println(Ejercicio_4.invertircadena("Recursividad"));
     }
 }
